b5ff2d6a4663e7ed405dd13374119f2a80621710
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-07-14 15:50:41 -0400

    buttons work and call gifs. Add gif button works, creates new gif button which also calls gifs when clicked

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -1,23 +1,53 @@
 $(document).ready(function () {
 
-var gifs = ["Halo" , "Super Mario" , "Just Cause" , "Final Fantasy"];
+var gifs = ["Halo" , "Super Mario" , "Yoshi" , "Final Fantasy", "Pokemon"];
 
 
 function test(){
     console.log("test")
-    // // make gif = to whatever button was pushed
-    // var gif = $(this).attr("data-name");
 
-    // //creating a URL to search giphy for the name on the button
-    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    //     gif + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-    //     // AJAX request
-    //     $.ajax({
-    //         url: queryURL,
-    //         method: "GET"
-    //       })
+    // make gif = to whatever button was pushed
+    var gif = $(this).attr("data-name");
 
-}
+    //creating a URL to search giphy for the name on the button
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        gif + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+        // AJAX request
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+          .then(function(response) {
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    //create a div for the gif
+                    var gifDiv = $("<div>");
+
+                    //variable to hold the rating
+                    var rating = results[i].rating;
+
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    // Creating an image tag and give it a src attribute of a proprty pulled off the result
+                    var gifImage = $("<img>").attr("src", results[i].images.fixed_height.url);
+
+                    //appending the rating and image to the gifDiv
+                    gifDiv.append(p);
+                    gifDiv.append(gifImage);
+
+                    // Prepending the gifDiv to the "#gif-view" div in the HTML
+                    $("#gif-view").prepend(gifDiv);
+
+
+                }//end appropriate rating check
+            }//end for results.length
+
+          }); //end .then function
+
+}//end test function
 
 
 
@@ -35,7 +65,22 @@ function renderButtons(){
         // Adding the button to the buttons-view div
         $("#buttons-view").append(a);
     }
-}
+}//end render button
+
+// This function handles events where a movie button is clicked
+$("#add-gif").on("click", function(event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var gif = $("#gif-input").val().trim();
+
+    // Adding movie from the textbox to our array
+    gifs.push(gif);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+});//end add gif
+
+
 //event listener on all buttons with the class gif-btn
 $(document).on("click", ".gif-btn", test);
 
