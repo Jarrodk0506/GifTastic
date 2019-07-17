7c9d28b5f802d39cc0410b476ead6d4f8444d812
Jarrod Krasnoff <Jarrodk0506@outlook.com>
2019-07-17 16:32:40 -0400

    Got gifs to console log on click and change data-state between still and animate, need help getting the images to actually change from still to animated

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-var gifs = ["Halo" , "Super Mario" , "Yoshi" , "Final Fantasy", "Pokemon"];
+var gifs = ["Borderlands" , "Super Mario" , "Yoshi" , "Final Fantasy", "Pokemon"];
 
 
 ////////////////////////////////
@@ -15,6 +15,8 @@ var gifs = ["Halo" , "Super Mario" , "Yoshi" , "Final Fantasy", "Pokemon"];
 
 
 
+
+
 function displayGif(){
 
     $("#gif-view").empty();
@@ -23,7 +25,7 @@ function displayGif(){
 
     //creating a URL to search giphy for the name on the button
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        gif + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+        gif + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=20";
         // AJAX request
         $.ajax({
             url: queryURL,
@@ -33,26 +35,53 @@ function displayGif(){
             var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
-                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                if (results[i].rating !== "r" ) {
                     //create a div for the gif
                     var gifDiv = $("<div>");
 
                     // Creating an image tag and give it a src attribute of a proprty pulled off the result
-                    var gifImage = $("<img>").attr("src", results[i].images.fixed_height.url);
+                    var gifImageStill = $("<img>").attr("src", results[i].images.fixed_height_still.url);
+                    gifImageStill.attr("data-state", "still");
+
+                    var gifImageAnimate = $("<img>").attr("src", results[i].images.fixed_height.url);
+                    gifImageAnimate.attr("data-state", "animate");
+
+                    var gifImage = $("<img>").attr("src", results[i].images.fixed_height_still.url);
+                    gifImage.attr("class", "gifImage");
+                    gifImage.attr("data-state", "still")
 
                     //appending the image to the gifDiv
                     gifDiv.append(gifImage);
 
                     // Prepending the gifDiv to the "#gif-view" div in the HTML
-                    $("#gif-view").prepend(gifDiv);
+                    $("#gif-view").append(gifDiv);
 
-
+                    
                 }//end appropriate rating check
+                
             }//end for results.length
+            $(".gifImage").on("click",function(){
+                
+                var state = $(this).attr("data-state")
 
+                if(state === "still"){
+                    console.log(this);
+                    $(this).attr("data-state", "animate");
+                }
+                else if(state === "animate"){
+                    console.log(this);
+                    $(this).attr("data-state", "still");
+                }
+
+            });
+            
+            
           }); //end .then function
 
-}//end test function
+}//end display gif
+
+
+
 
 
 
